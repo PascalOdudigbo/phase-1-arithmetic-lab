46821bad414f0516e80f82757aa5b81fb4a092a1
Pascal Odudigbo <odudigbopascal@gmail.com>
2022-06-07 10:46:05 +0300

    final commit i hope

--- index.js
+++ index.js
@@ -1,2 +1,11 @@
 
 // Write your code here
+const num1 = 31;
+const num2 = 2
+const num3 = 11;
+const num4 = 7;
+const multiply = num1 * num2;
+
+const random = Number.parseInt(Math.ceil(Math.random()));
+const mod = num3 % num4;
+const max = Math.max(10, 20, 15, 17);
